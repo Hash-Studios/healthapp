2a930172402f1afd13ce6a43ffe1e8517cdb224a
Abhay Maurya <maurya.abhay30@gmail.com>
2020-08-01 13:44:21 +0530

    added data

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,41 @@
-import 'package:flutter/material.dart';
-import 'package:healthapp/home.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:healthapp/home.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+
+Box prefs;
+Directory dir;
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  getApplicationDocumentsDirectory().then(
+    (dir) async {
+      Hive.init(dir.path);
+      prefs = await Hive.openBox('prefs');
+      prefs.put('username', 'Abhay Maurya');
+      prefs.put('userimage',
+          'https://images.unsplash.com/photo-1593229874334-90d965f27c42?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=min-hius-r-ii4jKDL6Y-unsplash.jpg&w=1920');
+      prefs.put('age', '20');
+      prefs.put('weight', '85');
+      prefs.put('height', '181');
+      prefs.put('blood', 'O+');
+      prefs.put('pulse', '75');
+      prefs.put('stepsTotal', '58456');
+      prefs.put('stepsToday', '2546');
+      prefs.put('calTotal', '38456');
+      prefs.put('calToday', '783');
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+          .then(
+        (value) => {
+          runApp(
+            MyApp(),
+          ),
+        },
+      );
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
